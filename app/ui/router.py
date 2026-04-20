8bef129ce8e11f402a4117023b4dbf3463e42d3c
Abel Gómez Méndez <abelmetaltele@gmail.com>
2026-04-20 19:52:29 -0400

    mejoras en funcionamiento de vista de agregar recetas

--- app/ui/router.py
+++ app/ui/router.py
@@ -3,7 +3,8 @@ from ui.views.costos_view import build_calculadora_view
 from ui.views.materiales_view import build_materiales_view
 from ui.views.compras_view import build_lot_view
 from ui.views.recetas_view import build_recetas_view
-from ui.callbacks import calcular_click
+from ui.callbacks import calcular_click, agregar_receta_click, agregar_ingrediente_click
+from services.material_service import listar_materiales_service
 
 def get_view_by_index(index: int, page: ft.Page) -> ft.Control:
     """Devuelve la vista correspondiente al índice seleccionado en la barra de navegación lateral.
@@ -20,6 +21,6 @@ def get_view_by_index(index: int, page: ft.Page) -> ft.Control:
         0: lambda: build_calculadora_view(page, calcular_click),
         1: lambda: build_materiales_view(page),
         2: lambda: build_lot_view(page),
-        3: lambda: build_recetas_view(page)
+        3: lambda: build_recetas_view(page, listar_materiales_service(), agregar_receta_click, agregar_ingrediente_click)
     }
     return views.get(index, lambda: ft.Text("Vista no encontrada"))()
--- app/ui/views/recetas_view.py
+++ app/ui/views/recetas_view.py
@@ -1,18 +1,24 @@
 import flet as ft
-from services.material_service import listar_materiales_service
-from ui.callbacks import agregar_receta_click, agregar_ingrediente_click
 
 
 
-def build_recetas_view(page: ft.Page):
-    receta_actual = {"id": -1}
+def build_recetas_view(
+    page: ft.Page,
+    lista_materiales,
+    agregar_receta_cb,
+    agregar_ingrediente_cb
+    ):
+    def create_state():
+        return {
+            "receta_id": -1
+        }
+    state = create_state()
     nombre_input = ft.TextField(label="Nombre del producto")
     rendimiento_input = ft.TextField(label="Rendimiento (unidades que produce la receta)")
 
     resultado = ft.Text()
     ingredientes_container = ft.Column(visible=True)
-    lista_materiales = listar_materiales_service()
-    dropdown = ft.Dropdown(
+    materia_prima_input = ft.Dropdown(
         label="Selecciona un ingrediente (materia prima)",
         width=250,
         options=[
@@ -23,13 +29,12 @@ def build_recetas_view(page: ft.Page):
             for m in lista_materiales
         ]
     )
-    materia_prima_input = dropdown
     cantidad_input = ft.TextField(label="Cantidad")
     
     def on_agregar_ingrediente(e):
-        receta_id = receta_actual["id"]
+        receta_id = state["receta_id"]
         print(f"TIPO RECETA: {type(receta_id)}")
-        agregar_ingrediente_click(
+        agregar_ingrediente_cb(
             e,
             receta_id,
             materia_prima_input.value,
@@ -43,31 +48,33 @@ def build_recetas_view(page: ft.Page):
         
 
     def on_guardar(e):
-        receta_id = agregar_receta_click(
+        receta_id = agregar_receta_cb(
             e,
             nombre_input,
             rendimiento_input,
             resultado,
             page
         )
-        print(f"RECETA ID: {receta_id}")
-        # Si se guardó correctamente
         if receta_id:
-            receta_actual["id"] = receta_id
+            state["receta_id"] = receta_id
             # 1. Cambiamos el estado
             ingredientes_container.visible = True
+            btn_add.disabled = False
+        page.update()
         
+    btn_add=ft.ElevatedButton(
+    "Agregar ingrediente",
+    on_click=on_agregar_ingrediente,
+    disabled=True
+    )    
 
     ingredientes_container.controls = [
     ft.Text("Agregar ingredientes a la receta"),
 
     materia_prima_input,
     cantidad_input,
+    btn_add
 
-    ft.ElevatedButton(
-        "Agregar ingrediente",
-        on_click=on_agregar_ingrediente
-    )
     ]
 
     boton = ft.ElevatedButton("Guardar", on_click=on_guardar)
@@ -80,7 +87,7 @@ def build_recetas_view(page: ft.Page):
             boton,
             resultado,
             ft.Divider(),  # línea visual
-            ingredientes_container  # 👈 aparece después de guardar
+            ingredientes_container 
         ],
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         alignment=ft.MainAxisAlignment.CENTER
